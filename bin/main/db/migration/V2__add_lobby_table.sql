0e4262aeb810c7bb5307bfc65ce7b46814e7d6aa
sabi1125 <sabir_barahi4@hotmail.com>
2026-01-31 10:08:32 +0900

    fix: removed generated bin and added it to gitignore

--- bin/main/db/migration/V2__add_lobby_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE lobby (
-    id VARCHAR(36) PRIMARY KEY,
-    game_settings_id VARCHAR(36) NOT NULL,
-    player_ids VARCHAR ARRAY NOT NULL,
-    is_game_started BOOLEAN NOT NULL,
-    FOREIGN KEY (game_settings_id) REFERENCES game_settings(id)
-);
