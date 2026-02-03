9a1379a9b3a186878582c2511cabbfc57786aefa
sabi1125 <sabir_barahi4@hotmail.com>
2026-02-03 22:21:20 +0900

    add: all required tables added

--- src/main/resources/db/migration/V1__initialize_tables.sql
+++ src/main/resources/db/migration/V1__initialize_tables.sql
@@ -3,3 +3,45 @@ CREATE TABLE player (
     username VARCHAR(50) NOT NULL UNIQUE
 );
 
+CREATE TABLE room (
+    id VARCHAR(36) NOT NULL,
+    host_player_id VARCHAR(36) NOT NULL,
+    is_game_started BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    FOREIGN KEY (host_player_id) REFERENCES player(id) ON DELETE CASCADE
+);
+
+CREATE TABLE game_settings (
+    id VARCHAR(36) NOT NULL,
+    room_id VARCHAR(36) NOT NULL,
+    max_players INT NOT NULL,
+    round_duration INT NOT NULL,
+    number_of_rounds INT NOT NULL,
+    language VARCHAR(20) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (room_id) REFERENCES room(id) ON DELETE CASCADE
+);
+
+CREATE TABLE room_player (
+    room_id VARCHAR(36) NOT NULL,
+    player_id VARCHAR(36) NOT NULL,
+    joined_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (room_id, player_id),
+    FOREIGN KEY (room_id) REFERENCES room(id) ON DELETE CASCADE,
+    FOREIGN KEY (player_id) REFERENCES player(id) ON DELETE CASCADE
+);
+
+CREATE TABLE categories (
+    game_setting_id VARCHAR(36) NOT NULL, 
+    category VARCHAR(50) NOT NULL,
+    PRIMARY KEY (game_setting_id),
+    FOREIGN KEY (game_setting_id) REFERENCES game_settings(id) ON DELETE CASCADE
+);
+
+CREATE TABLE EXCLUDED_LETTERS (
+    game_setting_id VARCHAR(36) NOT NULL, 
+    letters VARCHAR(50) NOT NULL,
+    PRIMARY KEY (game_setting_id),
+    FOREIGN KEY (game_setting_id) REFERENCES game_settings(id) ON DELETE CASCADE
+);
