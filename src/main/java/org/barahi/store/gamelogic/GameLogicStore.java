0cabb9df13c60cae57e03a818145aaebd680fcf4
michellebello <michellebelloveramendi@gmail.com>
2026-02-05 15:08:12 -0500

    added game logic store

--- src/main/java/org/barahi/store/gamelogic/GameLogicStore.java
+++ src/main/java/org/barahi/store/gamelogic/GameLogicStore.java
@@ -1,4 +1,15 @@
 package org.barahi.store.gamelogic;
 
+import jakarta.inject.Inject;
+import org.barahi.infra.DSLContextProvider;
+import org.jooq.DSLContext;
+
 public class GameLogicStore {
+  private final DSLContext db;
+
+  @Inject
+  public GameLogicStore(DSLContextProvider dbProvider){
+    this.db = dbProvider.get();
+  }
+
 }
