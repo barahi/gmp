0fa462432fc0cadbc08e0e541c468abc9bbd74c4
michellebello <michellebelloveramendi@gmail.com>
2026-02-05 17:20:49 -0500

    added round logic store

--- src/main/java/org/barahi/store/gamelogic/RoundLogicStore.java
+++ src/main/java/org/barahi/store/gamelogic/RoundLogicStore.java
@@ -5,7 +5,9 @@ import org.barahi.infra.DSLContextProvider;
 import org.barahi.serviceapi.player.Player.PlayerId;
 import org.jooq.DSLContext;
 
+import java.util.List;
 import java.util.Map;
+import static org.barahi.generated.Tables.*;
 
 public class RoundLogicStore {
   private final DSLContext db;
@@ -13,6 +15,36 @@ public class RoundLogicStore {
   @Inject
   public RoundLogicStore(DSLContextProvider dbProvider) {
     this.db = dbProvider.get();
+  }
+
+  public void storeAnswers(Map<String, String> playerAnswers, String category){
+    for (Map.Entry<String, String> answer: playerAnswers.entrySet()){
+      db.update(PLAYER_ANSWER)
+        .set(PLAYER_ANSWER.PLAYER_ID, answer.getKey())
+        .set(PLAYER_ANSWER.ANSWER, answer.getValue())
+        .where(PLAYER_ANSWER.CATEGORY.eq(category))
+        .execute();
+    }
+  }
+
+  public void storePlayerScores(Map<String, Integer> playerScores, String category){
+    for (Map.Entry<String, Integer> score: playerScores.entrySet()){
+      db.update(PLAYER_ANSWER)
+        .set(PLAYER_ANSWER.PLAYER_ID, score.getKey())
+        .set(PLAYER_ANSWER.SCORE, score.getValue())
+        .where(PLAYER_ANSWER.CATEGORY.eq(category))
+        .execute();
+    }
+  }
+
+  public Map<String, Integer> endRound(String category){
+    return db.select(PLAYER_ANSWER.PLAYER_ID, PLAYER_ANSWER.SCORE)
+      .from(PLAYER_ANSWER)
+      .where(PLAYER_ANSWER.CATEGORY.eq(category))
+      .fetchMap(
+        PLAYER_ANSWER.PLAYER_ID,
+        PLAYER_ANSWER.SCORE
+      );
   }
 
 }
