5324241bf1b29fdb9113216ea28c1c5e8e6d8eea
michellebello <michellebelloveramendi@gmail.com>
2026-08-17 18:58:54 -0400

    removed and cleaned up debugging lines

--- src/main/java/org/barahi/store/gamelogic/CumulativeScoreStore.java
+++ src/main/java/org/barahi/store/gamelogic/CumulativeScoreStore.java
@@ -20,20 +20,17 @@ public class CumulativeScoreStore {
   }
 
   public void initializeScores(RoomId roomId, List<PlayerId> playerIds) {
-    List<Query> queries = new ArrayList<>();
     playerIds.forEach(playerId -> {
-      queries.add(
         db.insertInto(CUMULATIVE_SCORE)
           .set(CUMULATIVE_SCORE.PLAYER_ID, playerId.getId().toString())
           .set(CUMULATIVE_SCORE.ROOM_ID, roomId.getId().toString())
           .set(CUMULATIVE_SCORE.SCORE, 0)
-          .onDuplicateKeyIgnore()
-      );
+          .onDuplicateKeyIgnore().execute();
     });
   }
 
   public Map<String, Integer> getPlayerScores(RoomId roomId) {
-      Map<String, Integer> roundResults =  db.select(PLAYER.USERNAME, CUMULATIVE_SCORE.SCORE, CUMULATIVE_SCORE.PLAYER_ID)
+    return db.select(PLAYER.USERNAME, CUMULATIVE_SCORE.SCORE, CUMULATIVE_SCORE.PLAYER_ID)
         .from(CUMULATIVE_SCORE)
         .join(PLAYER).on(CUMULATIVE_SCORE.PLAYER_ID.eq(PLAYER.ID))
         .where(CUMULATIVE_SCORE.ROOM_ID.eq(roomId.getId().toString()))
@@ -41,19 +38,16 @@ public class CumulativeScoreStore {
           r -> r.get(PLAYER.USERNAME),
           r -> r.get(CUMULATIVE_SCORE.SCORE) != null ? r.get(CUMULATIVE_SCORE.SCORE) : 0
         );
-      return roundResults;
+
   }
 
 
   public void updatePlayerScores(RoomId roomId, Map<PlayerId, Integer> prevRoundScoreMap) {
-    List<Query> queries = new ArrayList<>();
     for (Map.Entry<PlayerId, Integer> entry : prevRoundScoreMap.entrySet()) {
-      queries.add(
         db.update(CUMULATIVE_SCORE)
           .set(CUMULATIVE_SCORE.SCORE, CUMULATIVE_SCORE.SCORE.plus(entry.getValue()))
           .where(CUMULATIVE_SCORE.PLAYER_ID.eq(entry.getKey().getId().toString()))
-          .and(CUMULATIVE_SCORE.ROOM_ID.eq(roomId.getId().toString()))
-      );
+          .and(CUMULATIVE_SCORE.ROOM_ID.eq(roomId.getId().toString())).execute();
     }
   }
 
